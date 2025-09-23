65c25a42c028f6d4655bf5ec0a74b27338ec45b3
cjh15hub <cjh15@pct.edu>
2025-09-23 15:05:30 -0400

    small import fixes

--- deviljars/src/main/java/com/dudenduke/hudalternatives/deviljars/ClientEvents.java
+++ deviljars/src/main/java/com/dudenduke/hudalternatives/deviljars/ClientEvents.java
@@ -2,8 +2,11 @@ package com.dudenduke.hudalternatives.deviljars;
 
 import net.minecraft.client.Minecraft;
 import net.minecraft.resources.ResourceLocation;
+import net.neoforged.api.distmarker.Dist;
+import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.fml.common.Mod;
 import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
 import net.neoforged.neoforge.client.event.RegisterGuiLayersEvent;
 import net.neoforged.neoforge.client.gui.ConfigurationScreen;
@@ -11,9 +14,9 @@ import net.neoforged.neoforge.client.gui.IConfigScreenFactory;
 import net.neoforged.neoforge.client.gui.VanillaGuiLayers;
 
 // This class will not load on dedicated servers. Accessing client side code from here is safe.
-@net.neoforged.fml.common.Mod(value = Constants.MODID, dist = net.neoforged.api.distmarker.Dist.CLIENT)
+@Mod(value = Constants.MODID, dist = Dist.CLIENT)
 // You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
-@EventBusSubscriber(modid = Constants.MODID, value = net.neoforged.api.distmarker.Dist.CLIENT)
+@EventBusSubscriber(modid = Constants.MODID, value = Dist.CLIENT)
 public class ClientEvents {
     public ClientEvents(ModContainer container) {
         // Allows NeoForge to create a config screen for this mod's configs.
@@ -22,7 +25,7 @@ public class ClientEvents {
         container.registerExtensionPoint(IConfigScreenFactory.class, ConfigurationScreen::new);
     }
 
-    @net.neoforged.bus.api.SubscribeEvent
+    @SubscribeEvent
     static void onRegisterGuiOverlay(RegisterGuiLayersEvent event) {
         event.replaceLayer(ResourceLocation.fromNamespaceAndPath("minecraft", "player_health"), DevilJarsHudOverlay::emptyRender);
         event.replaceLayer(ResourceLocation.fromNamespaceAndPath("minecraft", "food_level"), DevilJarsHudOverlay::emptyRender);
@@ -30,7 +33,7 @@ public class ClientEvents {
         event.registerBelow(VanillaGuiLayers.HOTBAR, DevilJarsHudOverlay.DEVIL_JARS, DevilJarsHudOverlay::render);
     }
 
-    @net.neoforged.bus.api.SubscribeEvent
+    @SubscribeEvent
     static void onClientSetup(FMLClientSetupEvent event) {
         // Some client setup code
         DevilJarsHudMod.LOGGER.info("HELLO FROM CLIENT SETUP");
